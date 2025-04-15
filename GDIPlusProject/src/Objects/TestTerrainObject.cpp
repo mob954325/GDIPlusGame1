0b1843954ef04404ae5e9b8b920a47acd9abe353
mob954325 <lsh954325@gmail.com>
2025-04-15 13:46:30 +0900

    SetCollider 작성완료

--- GDIPlusProject/src/Objects/TestTerrainObject.cpp
+++ GDIPlusProject/src/Objects/TestTerrainObject.cpp
@@ -3,8 +3,8 @@
 TestTerrainObject::TestTerrainObject()
 {
 	tileSprite = {};
-	collider = {};
-	collider2 = {};
+	//collider = {};
+	collider = nullptr;
 }
 
 TestTerrainObject::~TestTerrainObject()
@@ -21,19 +21,18 @@ void TestTerrainObject::Initialize()
 
 	AddComponet(tileSprite);
 
-	collider = new SpriteCollider();
-	collider2 = new SpriteCollider();
+	// 콜라이더
+	collider = new SpriteCollider*[5];
 
 	// 임시
-	int top = tileSprite->imageHeight * 8;
-	int right = tileSprite->imageWidth * 10;
-	int bottom = tileSprite->imageHeight * 10;
+	SetCollider();
+	//int top = tileSprite->imageHeight * 8;
+	//int right = tileSprite->imageWidth * 10;
+	//int bottom = tileSprite->imageHeight * 10;
 
-	collider->bound = { 0, tileSprite->imageHeight * 10, tileSprite->imageWidth * 10, tileSprite->imageHeight * 9 };
-	collider2->bound = { 0, tileSprite->imageHeight * 8, tileSprite->imageWidth * 6, tileSprite->imageHeight * 7 };
+	//collider->bound = { 0, tileSprite->imageHeight * 10, tileSprite->imageWidth * 10, tileSprite->imageHeight * 9 };
 	//collider->bound = { 0, 20, 20, 0};
-	AddComponet(collider);
-	AddComponet(collider2);
+	//AddComponet(collider);
 }
 
 void TestTerrainObject::Update()
@@ -43,9 +42,9 @@ void TestTerrainObject::Update()
 
 void TestTerrainObject::Render(Gdiplus::Graphics* graphics)
 {
-	for (int y = 0; y < 10; y++)
+	for (int y = 0; y < tilesYCount; y++)
 	{
-		for (int x = 0; x < 10; x++)
+		for (int x = 0; x < tilesXCount; x++)
 		{
 			if (tiles[y][x] == 1)
 			{
@@ -54,8 +53,10 @@ void TestTerrainObject::Render(Gdiplus::Graphics* graphics)
 		}
 	}
 
-	collider->RenderCollider(graphics);
-	collider2->RenderCollider(graphics);
+	for (int i = 0; i < blockCount; i++)
+	{
+		collider[i]->RenderCollider(graphics);
+	}
 }
 
 void TestTerrainObject::OnColliderOverlap(GameObject* other)
@@ -76,6 +77,46 @@ void TestTerrainObject::OnColliderExit(GameObject* other)
 	}
 }
 
+// 이건 완료
 void TestTerrainObject::SetCollider()
 {
+	// 가로만 생성하기 -> 일단 모든 지형이 높이가 1인 사각형일 때만 가정함
+	int dx[4] = { 1,-1,0,0 };
+	int dy[4] = { 0,0,1,-1 };
+
+	int colliderIndex = 0;
+	for (int y = 0; y < tilesYCount; y++)
+	{
+		bool isVaild = false;
+		int rightCount = 0;
+		for (int x = 0; x < tilesXCount; x++)
+		{
+			if (!isVaild && tiles[y][x] == 1)
+			{
+				collider[colliderIndex] = new SpriteCollider();
+				AddComponet(collider[colliderIndex]);
+				collider[colliderIndex]->bound = {x * tileSprite->imageWidth, 
+												(y + 1) * tileSprite->imageHeight, 
+												(x) * tileSprite->imageWidth, 
+												(y) * tileSprite->imageHeight};
+				isVaild = true;
+			}
+
+			if (isVaild && tiles[y][x] == 1) // x 방향으로 늘어남
+			{
+				rightCount++;
+			}
+
+			if (isVaild && (tiles[y][x] == 0 || x == tilesXCount - 1)) // 지형이 아닌 구역에 도달 or 맵 끝에 도달
+			{
+				// bound 설정
+				collider[colliderIndex]->bound.right += rightCount * tileSprite->imageWidth;
+				colliderIndex++;
+				rightCount = 0;
+				isVaild = false;
+			}
+		}
+	}
+	
+	int a = 0;
 }
